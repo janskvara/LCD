6d5ae4bf53883f521362fdcc5d62f4fe8bd41e3c
Jan <jano.skvara@outlook.sk>
2019-01-24 15:56:09 +0100

    Add project files.

---
+++ LCD/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace LCD
+{
+    partial class Vescon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.time = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.RB1_ERROR = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Font = new System.Drawing.Font("MS Reference Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.time.Location = new System.Drawing.Point(5, 22);
+            this.time.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(242, 82);
+            this.time.TabIndex = 0;
+            this.time.Text = "00:00";
+            this.time.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // date
+            // 
+            this.date.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.date.Font = new System.Drawing.Font("MS Reference Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.date.Location = new System.Drawing.Point(1459, 27);
+            this.date.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.date.Name = "date";
+            this.date.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.date.Size = new System.Drawing.Size(442, 72);
+            this.date.TabIndex = 1;
+            this.date.Text = "00.00.2000";
+            this.date.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.date.UseMnemonic = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.ImageLocation = "C:\\Users\\SKVARA\\Desktop\\tabulka.jpg";
+            this.pictureBox1.Location = new System.Drawing.Point(19, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1875, 942);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(194, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(581, 76);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "AKTUÁLNY STAV";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(1156, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(637, 76);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PLÁNOVANÝ STAV";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(210, 434);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 76);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ĽS";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(652, 434);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 76);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "PS";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(1119, 434);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 76);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "ĽS";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(1596, 434);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 76);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "PS";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(74, 656);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 76);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "OK";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(547, 656);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(130, 76);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "OK";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(754, 656);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(178, 76);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "NOK";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.ForeColor = System.Drawing.Color.Black;
+            this.label10.Location = new System.Drawing.Point(289, 656);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(178, 76);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "NOK";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BackgroundImage = global::LCD.Properties.Resources.linka;
+            this.panel1.Controls.Add(this.RB1_ERROR);
+            this.panel1.Location = new System.Drawing.Point(12, 107);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1884, 942);
+            this.panel1.TabIndex = 13;
+            // 
+            // RB1_ERROR
+            // 
+            this.RB1_ERROR.BackColor = System.Drawing.Color.Transparent;
+            this.RB1_ERROR.Location = new System.Drawing.Point(211, 443);
+            this.RB1_ERROR.Name = "RB1_ERROR";
+            this.RB1_ERROR.Size = new System.Drawing.Size(193, 205);
+            this.RB1_ERROR.TabIndex = 1;
+            this.RB1_ERROR.Paint += new System.Windows.Forms.PaintEventHandler(this.RB1_ERROR_Paint);
+            // 
+            // Vescon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1908, 1061);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.time);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Vescon";
+            this.Text = "Zobrazovaci panel Vescon";
+            this.TransparencyKey = System.Drawing.SystemColors.ControlDark;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Label date;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label RB1_ERROR;
+    }
+}
+
---
+++ LCD/Form1.cs
@@ -0,0 +1,207 @@
+﻿using Microsoft.Win32;
+using S7.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace LCD
+{
+    public partial class Vescon : Form
+    {
+        
+        private void RB1_ERROR_Paint(object sender, PaintEventArgs e)
+        {
+            Pen skyBluePen = new Pen(Brushes.Red);
+
+            // Set the pen's width.
+            skyBluePen.Width = 10.0F;
+
+            // Set the LineJoin property.
+            skyBluePen.LineJoin = System.Drawing.Drawing2D.LineJoin.Bevel;
+
+            // Draw a rectangle.
+            e.Graphics.DrawRectangle(skyBluePen,
+                new Rectangle(0, 0, RB1_ERROR.Width, RB1_ERROR.Height));
+
+            //Dispose of the pen.
+            skyBluePen.Dispose();
+        }
+
+        private bool OK_L,NOK_L,OK_P, NOK_P ,plan_L,plan_P,chyba_1RB1,chyba_2KT,chyba_3RP1,chyba_3LD1,chyba_4RB2,chyba_5RP24,
+                       chyba_5RP23, chyba_5RP22, chyba_5RP21, chyba_5LD2,chyba_6VK;
+
+
+        private bool  upozornenie_1RB1, upozornenie_2KT, upozornenie_3RP1, upozornenie_3LD1, upozornenie_4RB2, upozornenie_5RP24,
+                       upozornenie_5RP23, upozornenie_5RP22, upozornenie_5RP21, upozornenie_5LD2, upozornenie_6VK;
+
+        public Vescon()
+        {
+            InitializeComponent();
+
+            Timer Timer1 = new Timer();
+            Timer Timer2 = new Timer();
+            Timer Timer3 = new Timer();
+            Timer2.Interval = 10000;
+            Timer1.Interval = 1000;
+            Timer3.Interval = 500;
+            Timer2.Tick += new EventHandler(Timer2_Tick);
+            Timer2.Enabled = true;
+            Timer1.Tick += new EventHandler(Timer1_Tick);
+            Timer1.Enabled = true;
+            Timer3.Tick += new EventHandler(Timer3_Tick);
+            Timer3.Enabled = true;
+
+
+
+        }
+
+        
+        private void Timer1_Tick(object Sender, EventArgs e)
+        {
+
+            //Plc plc = new Plc(CpuType.S7300, "192.168.0.10", 0, 2);
+            //plc.Open();
+            //    OK_L = (bool)plc.Read("DB616.DBX0.0");
+            //    NOK_L = (bool)plc.Read("DB616.DBX0.1");
+            //    OK_P = (bool)plc.Read("DB616.DBX0.2");
+            //    NOK_P= (bool)plc.Read("DB616.DBX0.3");
+            //    plan_L = (bool)plc.Read("DB616.DBX0.4");
+            //    plan_P= (bool)plc.Read("DB616.DBX0.5");
+
+            //    chyba_1RB1 = (bool)plc.Read("DB616.DBX18.0");
+            //    chyba_2KT = (bool)plc.Read("DB616.DBX18.1");
+            //    chyba_3RP1 = (bool)plc.Read("DB616.DBX18.2");
+            //    chyba_3LD1 = (bool)plc.Read("DB616.DBX18.3");
+            //    chyba_4RB2 = (bool)plc.Read("DB616.DBX18.4");
+            //    chyba_5RP24 = (bool)plc.Read("DB616.DBX18.5");
+            //    chyba_5RP23 = (bool)plc.Read("DB616.DBX18.6");
+            //    chyba_5RP22 = (bool)plc.Read("DB616.DBX18.7");
+            //    chyba_5RP21 = (bool)plc.Read("DB616.DBX19.0");
+            //    chyba_5LD2 = (bool)plc.Read("DB616.DBX19.1");
+            //    chyba_6VK = (bool)plc.Read("DB616.DBX19.2");
+
+            //    upozornenie_1RB1 = (bool)plc.Read("DB616.DBX19.3");
+            //    upozornenie_2KT = (bool)plc.Read("DB616.DBX19.4");
+            //    upozornenie_3RP1 = (bool)plc.Read("DB616.DBX19.5");
+            //    upozornenie_3LD1 = (bool)plc.Read("DB616.DBX19.6");
+            //    upozornenie_4RB2 = (bool)plc.Read("DB616.DBX19.7");
+            //    upozornenie_5RP24 = (bool)plc.Read("DB616.DBX20.0");
+            //    upozornenie_5RP23 = (bool)plc.Read("DB616.DBX20.1");
+            //    upozornenie_5RP22 = (bool)plc.Read("DB616.DBX20.2");
+            //    upozornenie_5RP21 = (bool)plc.Read("DB616.DBX20.3");
+            //    upozornenie_5LD2 = (bool)plc.Read("DB616.DBX20.4");
+            //    upozornenie_6VK = (bool)plc.Read("DB616.DBX20.5");
+
+                //ushort pomocna_int = (ushort)PLC.Read(okna[i]);
+                //ushort pomocna_int = (ushort)PLC.Read(okna[i]);
+                //ushort pomocna_int = (ushort)PLC.Read(okna[i]);
+                //ushort pomocna_int = (ushort)PLC.Read(okna[i]);
+                //ushort pomocna_int = (ushort)PLC.Read(okna[i]);
+            //plc.Close();
+
+            time.Text = DateTime.Now.ToString("HH:mm", System.Globalization.DateTimeFormatInfo.InvariantInfo);
+            date.Text = DateTime.Now.ToShortDateString();
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\VesconSettings");
+            String display = key.GetValue("DISPLAY").ToString();
+            showOnScreen(display, true);
+
+           
+        }
+        private void Timer2_Tick(object Sender, EventArgs e)
+        {
+            if(panel1.Visible == false)
+            {
+                panel1.Visible = true; 
+            }
+            else
+            {
+                panel1.Visible = false;
+            }
+           
+
+        }
+
+        private void Timer3_Tick(object Sender, EventArgs e)
+        {
+            
+           
+            if (panel1.Visible == true)
+            {
+                blikanie(RB1_ERROR, false, true);
+            }
+
+        }
+
+
+
+
+        void showOnScreen(string screenName, bool maximised = false)
+        {
+            try
+            {
+                Screen res = Screen.AllScreens.FirstOrDefault(s => s.DeviceName == screenName);
+                Location = res.WorkingArea.Location;
+                if (maximised)
+                {
+                    FormBorderStyle = FormBorderStyle.None;
+                    WindowState = FormWindowState.Maximized;
+                    TopMost = true;
+                }
+                else
+                {
+                    FormBorderStyle = FormBorderStyle.FixedSingle;
+                    WindowState = FormWindowState.Normal;
+                    TopMost = false;
+                }
+            }
+            catch
+            {
+                FormBorderStyle = FormBorderStyle.FixedSingle;
+                WindowState = FormWindowState.Normal;
+                TopMost = false;
+                MessageBox.Show("DISPLAY NIE JE PRIPOJENY");
+            }
+               
+            
+        }
+
+        void blikanie( Label blikanie , bool error ,bool warning )
+        {
+            if (blikanie.Visible == false)
+            {
+                if (error)
+                {
+                    
+                    //blikanie.BackColor = Color.Red;
+                    blikanie.Visible = true;
+                }
+                else if (warning)
+                {
+                    
+                    //blikanie.BackColor = Color.Yellow;
+                    blikanie.Visible = true;
+                }
+                else
+                {
+
+                }
+               
+            }
+            else
+            {
+                blikanie.Visible = false;
+            }
+
+        }
+
+
+    }
+}
